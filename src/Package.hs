deab184123b3353fc62eec42fe5544ccbe02a14a
Mayorov Andrey <avmayorov@platbox.com>
2014-09-10 18:04:28 +0400

    Fix version comparison

--- src/Package.hs
+++ src/Package.hs
@@ -15,12 +15,11 @@ module Package (
     platform
 ) where
 
-import Prelude
+import Prelude hiding (null, init, tail, length)
 import Data.Function (on)
 import Data.Ord (comparing)
 import Data.Monoid
-import Data.Text (Text)
-import qualified Data.Text as Text
+import Data.Text (Text, null, empty, init, tail, length, breakOn, breakOnEnd, split)
 
 data Section = Testing | Stable
     deriving (Show, Read, Enum, Bounded, Eq, Ord)
@@ -47,31 +46,39 @@ type PackageList = [Package]
 
 fromSectionFullName :: Section -> Text -> Maybe Package
 fromSectionFullName sec s =
-    case Text.split (== '_') s of
+    case split (== '_') s of
         [n, v, p] -> Just $ Package sec s n v p
         _ -> Nothing
 
 compareVersion :: Version -> Version -> Ordering
 compareVersion = comparing dissect
 
-dissect :: Version -> ([Text], [Text])
-dissect subject = (epoch : parts, leftover) where
-    (epoch, rest) = let (e, r) = Text.breakOn ":" subject in
-        if Text.null r
-            then ("0", subject)
-            else (e, safeTail r)
-    (parts, leftover) = let (p, r) = Text.breakOnEnd "-" rest in
-        if Text.null p
-            then (parts' r, [])
-            else (parts' $ safeInit p, parts' r)
-    parts' s = take maxParts $ Text.split (flip elem $ delimeters) s ++ partsTail
-    partsTail = repeat Text.empty
-    safeInit = safeOn Text.init
-    safeTail = safeOn Text.tail
+newtype VersionHunk = VersionHunk Text
+    deriving (Eq)
+
+instance Ord VersionHunk where
+    compare (VersionHunk h1) (VersionHunk h2) =
+        comparing length h1 h2 <> compare h1 h2
+
+dissect :: Version -> ([VersionHunk], [VersionHunk])
+dissect subject = (epoch : hunks, leftover) where
+    (epoch, rest) = let (e, r) = breakOn ":" subject in
+        if null r
+            then (VersionHunk "0" , subject)
+            else (VersionHunk e   , safeTail r)
+    (hunks, leftover) = let (p, r) = breakOnEnd "-" rest in
+        if null p
+            then (hunks' r, [])
+            else (hunks' $ safeInit p, hunks' r)
+    hunks' s = fmap VersionHunk $
+        take maxHunks $ split (flip elem $ delimeters) s ++ hunksLeft
+    hunksLeft = repeat empty
+    safeInit = safeOn init
+    safeTail = safeOn tail
     delimeters = ".+-:~"
-    maxParts = 8
+    maxHunks = 8
 
 safeOn :: (Text -> Text) -> Text -> Text
 safeOn f s
-    | Text.null s = s
+    | null s = s
     | otherwise = f s
